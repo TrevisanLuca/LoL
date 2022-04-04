9e44cff9f72c39d286f388febecc200e994fb2ca
Luca Trevisan <trevisanluca86@gmail.com>
2022-04-04 20:49:09 +0200

    add JS script for team creation

--- LoL.MVC/wwwroot/Scripts/TeamCreation.js
+++ LoL.MVC/wwwroot/Scripts/TeamCreation.js
@@ -1,20 +1,48 @@
-﻿let button = document.getElementById("myButton");
-let inputSet = Array.from(document.getElementsByClassName("myInput"));
-let inputSet = new Set();
+﻿document.addEventListener("DOMContentLoaded", () => {
+    let button = document.getElementById("myButton");
+    let inputArray = Array.from(document.getElementsByClassName("myInput"));
+    let inputSet = new Set();
+    let oldPlayersCache = [];
+    let arrayCounter = 0;
 
 
-function checkButton() {
-    inputSet.clear();
-    inputArray.forEach(x => inputSet.add(x.value));
-    if (inputSet.size === 5) {
-        button.disabled = false;
+    function checkButton() {
+        inputSet.clear();
+        inputArray.forEach(x => inputSet.add(x.value));
+        if (inputSet.size === 5) {
+            button.disabled = false;
+        }
+        else {
+            button.disabled = true;
+        }
+    };
+
+function disableUsedPlayers(thisElement) {
+    let usedPlayer = thisElement.options[thisElement.options.selectedIndex].value;
+    for (element of inputArray) {
+        if (element != thisElement) {
+            for (option of element.options) {
+                if (option.value === usedPlayer) {
+                    option.hidden = true;
+                }
+                else if (option.value === oldPlayersCache[thisElement.arrayIndex]) {
+                    option.hidden = false;
+                }
+            }
+        }
     }
-    else {
-        button.disabled = true;
+    oldPlayersCache[thisElement.arrayIndex] = usedPlayer;
     }
-};
 
-for (inputElement of inputSet) {
-    inputElement.onselectionchange = checkButton;
-}
-checkButton();
+    //setUp
+    for (let inputElement of inputArray) {
+        inputElement.onchange = () => {
+            checkButton();
+            disableUsedPlayers(inputElement);
+        };
+        inputElement.arrayIndex = arrayCounter;
+        disableUsedPlayers(inputElement);
+        arrayCounter = arrayCounter + 1;
+    }
+    checkButton();
+});
